6d7d6ce2f31f29ae6671085be5501a999c5ec238
Roland Schmitt <rschmitt@samhammer.de>
2020-02-05 15:19:34 +0100

    Fixes #22 fixes typescript module definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,11 @@
-import Vue from 'vue';
+declare module 'vue-autonumeric' {
+    import Vue from 'vue';
 
-export default class VueAutonumeric extends Vue {
-    public value: number | null | string;
-    public options: object | string | object[] | string[];
-    public resetOnOptions: boolean;
-    public placeholder: string;
-    public tag: string;
+    export default class VueAutonumeric extends Vue {
+        public value: number | null | string;
+        public options: object | string | object[] | string[];
+        public resetOnOptions: boolean;
+        public placeholder: string;
+        public tag: string;
+    }
 }
