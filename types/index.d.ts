2b584f72108db56516d219ef20a0330dda5c1188
Roland Schmitt <rschmitt@samhammer.de>
2020-02-05 15:01:50 +0100

    Fixes #22 add typescript definitions

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+export default class VueAutonumeric extends Vue {
+    public value: number | null | string;
+    public options: object | string | object[] | string[];
+    public resetOnOptions: boolean;
+    public placeholder: string;
+    public tag: string;
+}
