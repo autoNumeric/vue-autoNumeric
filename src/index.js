5be659354ed51cead3c0b1d444cafc02bbcdffca
Eric Johansson <ericj_@hotmail.com>
2020-10-22 15:56:54 +0200

    cleaner export of component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import VueAutonumeric from './components/VueAutonumeric.vue';
 
 // Expose component to global scope
 /* eslint no-undef : 0 */
-if (typeof window !== 'undefined' && window.Vue) {
-    Vue.component('vue-autonumeric', VueAutonumeric);
-}
-
-export { VueAutonumeric };
 
 export default {
     install (app) {
